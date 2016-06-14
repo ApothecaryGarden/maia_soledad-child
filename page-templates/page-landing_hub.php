2c2e78352d530e1dbb730b02fc01bf5627bc8c2c
oakwoodgates <funkyoaktree@gmail.com>
2016-06-13 23:21:43 -0400

    update

--- page-templates/page-landing_hub.php
+++ page-templates/page-landing_hub.php
@@ -8,18 +8,12 @@
 ?>
 <?php get_header(); ?>
 
-<div class="container penci_sidebar left-sidebar">
+<div class="container">
 	<div id="main">
 		<div class="theiaStickySidebar">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<?php get_template_part('content', 'page'); ?>
 			<?php endwhile; endif; ?>
-			<?php
-
-			?>
 		</div>
-	</div>
-	<div id="sidebar">
-		<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'sidebar-hub' ) ) ?>
 	</div>
 <?php get_footer('basic');
